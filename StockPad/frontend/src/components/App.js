670b243fbc3d8ddf7bf0e1d1f96a48ab16c1095a
erickcruz1031@gmail.com <erickcruz1031@gmail.com>
2021-01-27 17:44:06 -0800

    Got the template working with Material UI

--- StockPad/frontend/src/components/App.js
+++ StockPad/frontend/src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom'
+import Dashboard from './Dashboard/Dashboard';
 
 export class App extends Component{
     render(){
         return(
-            <h1>this is the NOOOO</h1>
+            <Dashboard />
         )
     }
 }
@@ -13,3 +14,4 @@ const rootDiv = document.getElementById('root');
 render(<App />, rootDiv);
 
 //ReactDOM.render(<App />, document.getElementById('root'));
+//    <link rel="stylesheet" type="text/css" href="{% static "css/index.css" %}" />
