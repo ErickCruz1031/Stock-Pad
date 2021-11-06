4e098f45bd029c39af2601eb453bd18969b407d7
Erick  Jaimez <erickcruz1031@gmail.com>
2021-11-06 13:54:07 -0700

    Added an alert and check to make sure passwords on register form match. Fixed the flow so that the user can re-submit the Register request if the passwords don't initially match

--- StockPad/frontend/src/components/Signin/Register.js
+++ StockPad/frontend/src/components/Signin/Register.js
@@ -54,9 +54,11 @@ const Register = () => {
 
     const [username, setUsername] = useState("")
     const [pass, setPass] = useState("");
+    const [passTwo, setPassTwo] = useState("");//The second password entered used to confirm that they both match
     const [email, setEmail] = useState("");
     const [triggerCall, setTrigger] = useState(false); //Trigger that will be used for the API call 
     const [errorAlert, setErrorAlert] = useState(false); //At first the alert is not shown
+    const [passMismatch, setMismatch] = useState(false);//Variable that will set off alert if the passwords do not match
 
 
     const classes = useStyles();
@@ -66,32 +68,29 @@ const Register = () => {
     const pass_Ref = useRef();//Ref for password child
 
     const changeUsername = (e) =>{
-
       setUsername(e.target.value)
-    }
+    }//Function sets the username that will be send in the request
 
     const changePass = (e) =>{
       setPass(e.target.value);
       //When we submit we need to check whether the passwords in the two text fields match 
     }
 
+    const changeSecondPass = e => {
+      setPassTwo(e.target.value);//Set the second password which will be used when the user tries to send the request
+    }
+
     const changeEmail = e =>{
         setEmail(e.target.value);
-    }
+    }//Function sets the email that will be used when the user sends the request
 
     const submitCreds = (e) =>{
       e.preventDefault();
       console.log("Submitted the credentials");
-      console.log("These are the inputs ", email, ' ', pass)
-
       //Change the state of the trigger variable so that the useEffect will execute 
       setTrigger(true)
       user_Ref.current.value = "";
       pass_Ref.current.value = "";
-
-      console.log("Finished the calll")
-            
-
     }
 
     useEffect(() =>{
@@ -131,12 +130,22 @@ const Register = () => {
     
         } //Function to register the new user 
         if (triggerCall){
+          console.log("Going in these are the two passwords: ", pass, " and ", passTwo);
+          //Before we call the request, check if the two passwords match
+          if(pass == passTwo){
+            console.log("The two passwords match, making the call...");
+            setMismatch(true)//Toggle off the alert(Don't think this matters since we're switching the view)
             RegisterUser();
+          }//If the two passwords match make the call. Otherwise, throw out a warning 
+          else{
+            setMismatch(true)//If the passwords don't match, trigger the alert
+            setTrigger(false);//Reset the trigger for this call in case the user wants to re-submit the request
+
+          }//If they dont match, show a warning indicating that the passwords do not match
+          
         } //If triggerCall is true then we make the call. Otherwise we skip
     
-            
-      
-          
+     
 
     },[triggerCall]);//This is where the API call will be to create the new user 
 
@@ -199,6 +208,7 @@ const Register = () => {
           <TextField
             error = {true === errorAlert}
             ref={pass_Ref}
+            onChange = {changeSecondPass}
             variant="outlined"
             margin="normal"
             required
@@ -229,7 +239,7 @@ const Register = () => {
                                     
                   <Slide direction="up" in={errorAlert} mountOnEnter unmountOnExit>
                       
-                      <Alert severity="error">The username or password endetered are not valid. Please try again.</Alert>
+                      <Alert severity="error">The username, password and/or email entered are not valid. Please try again.</Alert>
                       
                   </Slide>
                   
@@ -237,6 +247,20 @@ const Register = () => {
                   <> </>
 
                 }
+                {passMismatch ?
+                                    
+                  <Slide direction="up" in={passMismatch} mountOnEnter unmountOnExit>
+                      
+                      <Alert severity="error">The two passwords entered did not match. Please try again.</Alert>
+                      
+                  </Slide>
+                  
+                  :
+                  <> </>
+                  
+                }
+
+
               </Box>
               <Box mt={8}>
                 <Copyright />
